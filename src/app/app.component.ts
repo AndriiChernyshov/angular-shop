be272b2c8d21252fcf73c51924aace56a5cdb8f5
Andrii Chernyshov <andriichernyshov@outlook.com>
2017-07-05 19:58:40 +0300

    simple properties

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  name = "Angular Shop site";
+  description = "Angular Test project";
+  price:number = 10;
+  category: CategoryTypes = CategoryTypes.Good;
+  isAvailable:boolean = true;
+
+  equivalents: number[] = [41,42,43,46];
+  ingredients: Array<string> = ["Not bad", "Good", "Wwwweee"];
+}
+
+enum CategoryTypes{
+  Good = 1,
+  Bad,
+  Ugly    
 }
